41973274a454009804e7fec06dc3b25f752375b6
Achraf333-m <achrafdaimallah2003@gmail.com>
2024-12-13 13:08:44 -0500

    updated hospital header

--- hospital/hospital.h
+++ hospital/hospital.h
@@ -11,11 +11,20 @@ private:
     std::vector<Patient> patient_v;
     std::vector<Doctor> doctor_v;
 
-    Patient *patient_ptr = &patient_v[0];
-    Doctor *doctor_ptr = &doctor_v[0];
+    std::vector<Patient> *patient_ptr = &patient_v;
+    std::vector<Doctor> *doctor_ptr = &doctor_v;
 
 public:
     Hospital();
+
+    // members
+    void find_oldest_patient();
+    int count_critical_patients();
+    void doctors_by_specialty(std::string specialty);
+    void show_patient_by_id(long int id);
+    void show_doctor_by_id(long int id);
+    void show_assigned_doctor(long int id);
+    void show_assigned0_patients(long int id);
 };
 
 #endif
