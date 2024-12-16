9282ba36614a7190b95585633ce86c976549e94e
Achraf333-m <achrafdaimallah2003@gmail.com>
2024-12-15 21:36:39 -0500

    format verification added

--- hospital_driver.cpp
+++ hospital_driver.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <limits>
 
-void make_selection(int&);
-void hospital_app(int, Hospital&);
+void make_selection(int &);
+void hospital_app(int, Hospital &);
 
-
-int main() {
+int main()
+{
     int user_selection{0};
     Hospital montreal_hospital;
 
@@ -17,9 +17,11 @@ int main() {
     return 0;
 }
 
-void make_selection(int &selection) {
+void make_selection(int &selection)
+{
     bool selecting = true;
-    while (selecting) {
+    while (selecting)
+    {
         std::cout << "\n\n\n          Welcome to your hospital app            " << "\n\n\n"
                   << "Please select one of the following options: \n"
                   << "1. Find the oldest patient.\n"
@@ -30,56 +32,115 @@ void make_selection(int &selection) {
                   << "6. Show assigned patients for a given doctor.\n"
                   << "7. Exit.\n"
                   << "Your selection: ";
-        
+
         std::cin >> selection;
 
-        if (std::cin.fail() || selection < 1 || selection > 7) {
-            std::cin.clear(); //clear the error flag
-            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); //discard invalid input
+        if (std::cin.fail() || selection < 1 || selection > 7)
+        {
+            std::cin.clear();                                                   // clear the error flag
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // discard invalid input
             std::cout << "Invalid option, please try again.\n";
-
-        } else {
-            selecting = false; //valid input, exit loop
+        }
+        else
+        {
+            selecting = false; // valid input, exit loop
         }
     }
 }
 
-void hospital_app(int selection, Hospital &montreal_hospital) {
+void hospital_app(int selection, Hospital &montreal_hospital)
+{
     long int id{0};
-    switch (selection) {
-        case 1:
-            montreal_hospital.find_oldest_patient();
-            break;
-        case 2:
-            std::cout << "\nThe number of critical patients is: " << montreal_hospital.count_critical_patients() << std::endl;
-            break;
-        case 3:
-            std::cout << "Enter patient ID: ";
+    switch (selection)
+    {
+    case 1:
+        montreal_hospital.find_oldest_patient();
+        break;
+    case 2:
+        std::cout << "\nThe number of critical patients is: " << montreal_hospital.count_critical_patients() << std::endl;
+        break;
+    case 3:
+        std::cout << "Enter patient ID: ";
+        try
+        {
             std::cin >> id;
-            std::cout << std::endl << "The patient with matching ID is: \n " << std::endl;
-            montreal_hospital.show_patient_by_id(id);
-            break;
-        case 4:
-            std::cout << "Enter doctor ID: ";
+            if (id >= 10000000 && id <= 99999999)
+            {
+                std::cout << std::endl;
+                montreal_hospital.show_patient_by_id(id);
+            }
+            else
+            {
+                throw std::runtime_error("The ID provided is not 8 digits long, please try again.");
+            }
+        }
+        catch (std::runtime_error m)
+        {
+            std::cout << m.what();
+        }
+        break;
+    case 4:
+        std::cout << "Enter doctor ID: ";
+        try
+        {
             std::cin >> id;
-            montreal_hospital.show_doctor_by_id(id);
-            break;
-        case 5:
-            std::cout << "Enter patient ID: ";
+            if (id >= 10000000 && id <= 99999999)
+            {
+                montreal_hospital.show_doctor_by_id(id);
+            }
+            else
+            {
+                throw std::runtime_error("The ID provided is not 8 digits long, please try again.");
+            }
+        }
+        catch (std::runtime_error m)
+        {
+            std::cout << m.what();
+        }
+        break;
+    case 5:
+        std::cout << "Enter patient ID: ";
+        try
+        {
             std::cin >> id;
-            montreal_hospital.show_assigned_doctor(id);
-            break;
-        case 6:
-            std::cout << "Enter doctor ID: ";
+            if (id >= 10000000 && id <= 99999999)
+            {
+                montreal_hospital.show_assigned_doctor(id);
+            }
+            else
+            {
+                throw std::runtime_error("The ID provided is not 8 digits long, please try again.");
+            }
+        }
+        catch (std::runtime_error m)
+        {
+            std::cout << m.what();
+        }
+        break;
+    case 6:
+        std::cout << "Enter doctor ID: ";
+        try
+        {
             std::cin >> id;
-            montreal_hospital.show_assigned_patients(id);
-            break;
-        case 7:
-            std::cout << "Have a good day!";
-            break;
-        default:
-            std::cout << "Something went wrong, please try again.";
-            break;
+            if (id >= 10000000 && id <= 99999999)
+            {
+                montreal_hospital.show_assigned_patients(id);
+            }
+            else
+            {
+                throw std::runtime_error("The ID provided is not 8 digits long, please try again.");
+            }
+        }
+        catch (std::runtime_error m)
+        {
+            std::cout << m.what();
+        }
+        break;
+    case 7:
+        std::cout << "Have a good day!";
+        break;
+    default:
+        std::cout << "Something went wrong, please try again.";
+        break;
     }
 }
-
